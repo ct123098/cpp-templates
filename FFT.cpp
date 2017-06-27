926bee6d9351b2e3589d7fd8c7d5c45c72971015
ct123098 <tom20000609@126.com>
2017-06-27 16:01:22 +0800

    update ntt and fft

--- FFT.cpp
+++ FFT.cpp
@@ -3,56 +3,41 @@
 #include <cstdlib>
 #include <cstring>
 #include <algorithm>
+#include <complex>
 #include <cmath>
-#include <vector>
 
 using namespace std;
 
 const int MAXN = 100000 + 100;
-const int MOD = 998244353;
-const int PRIMITIVE_ROOT = 3;
 
-int fpow(int x, int p)
+namespace myFFT
 {
-	if(p == 0) return 1;
-	int t = fpow(x, p / 2);
-	t = (long long)t * t % MOD;
-	if(p & 1) return (long long)t * x % MOD;
-	else return t;
-}
-
-int get_inv(int x)
-{
-	return fpow(x, MOD - 2);
-}
-
-namespace myNTT
-{
-	int rev[4 * MAXN], w1[4 * MAXN], w2[4 * MAXN];
-	void NTT_core(int n, int *a, int *w)
+	const double PI = acos(-1.0);
+	int rev[4 * MAXN];
+	complex<double> w1[4 * MAXN], w2[4 * MAXN];
+	void FFT_core(int n, complex<double> *a, complex<double> *w)
 	{
 		for(int i = 0; i < n; i++)
-			if(rev[i] < i)
-				swap(a[rev[i]], a[i]);
+			if(i < rev[i])
+				swap(a[i], a[rev[i]]);
 		for(int i = 1; i < n; i <<= 1)
 			for(int j = 0; j < n; j += 2 * i)
 				for(int k = 0; k < i; k++)
 				{
-					int x = a[j + k], y = (long long)a[j + k + i] * w[i + k] % MOD;
-					a[j + k] = (x + y) % MOD;
-					a[j + k + i] = (x - y + MOD) % MOD;
+					complex<double> x = a[j + k], y = a[i + j + k] * w[i + k];
+					a[j + k] = x + y;
+					a[i + j + k] = x - y;
 				}
 	}
-	void NTT(int n, int *a)
+	void DFT(int n, complex<double> *a)
 	{
-		NTT_core(n, a, w1);
+		FFT_core(n, a, w1);
 	}
-	void INTT(int n, int *a)
+	void IDFT(int n, complex<double> *a)
 	{
-		NTT_core(n, a, w2);
-		int inv = get_inv(n);
+		FFT_core(n, a, w2);
 		for(int i = 0; i < n; i++)
-			a[i] = (long long)a[i] * inv % MOD;
+			a[i] /= n;
 	}
 	void init(int n)
 	{
@@ -60,34 +45,28 @@ namespace myNTT
 		rev[0] = 0;
 		for(int i = 1; i < n; i++)
 			rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (l - 1));
-		int wn = fpow(PRIMITIVE_ROOT, (MOD - 1) / n);
-		w1[half] = 1;
-		for(int i = 1; i < half; i++)
-			w1[half + i] = (long long)w1[half + i - 1] * wn % MOD;
+		for(int i = 0; i < half; i++)
+			w1[half + i] = complex<double>(cos(2 * PI * i / n), sin(2 * PI * i / n));
 		for(int i = half - 1; i >= 0; i--)
 			w1[i] = w1[i << 1];
-		wn = get_inv(wn);
-		w2[half] = 1;
-		for(int i = 1; i < half; i++)
-			w2[half + i] = (long long)w2[half + i - 1] * wn % MOD;
-		for(int i = half - 1; i >= 0; i--)
-			w2[i] = w2[i << 1];
+		for(int i = 0; i < n; i++)
+			w2[i] = conj(w1[i]);
 	}
-	void conv_ntt(int n, int *a, int m, int *b, int *c)
+	void conv_fft(int n, int *a, int m, int *b, int *c)
 	{
-		static int A[4 * MAXN], B[4 * MAXN], C[4 * MAXN];
+		static complex<double> A[4 * MAXN], B[4 * MAXN], C[4 * MAXN];
 		int len = 1;
 		while(len < n + m - 1) len <<= 1;
-		for(int i = 0; i < len; i++) A[i] = B[i] = C[i] = 0;
+		for(int i = 0; i < len; i++) A[i] = B[i] = C[i] = complex<double>(0.0, 0.0);
 		for(int i = 0; i < n; i++) A[i] = a[i];
 		for(int i = 0; i < m; i++) B[i] = b[i];
 		init(len);
-		NTT(len, A), NTT(len, B);
+		DFT(len, A), DFT(len, B);
 		for(int i = 0; i < len; i++)
-			C[i] = (long long)A[i] * B[i] % MOD;
-		INTT(len, C);
+			C[i] = A[i] * B[i];
+		IDFT(len, C);
 		for(int i = 0; i < n + m - 1; i++)
-			c[i] = C[i];
+			c[i] = C[i].real() + 0.5;
 	}
 	void conv_force(int n, int *a, int m, int *b, int *c)
 	{
@@ -96,7 +75,7 @@ namespace myNTT
 			C[i] = 0;
 		for(int i = 0; i < n; i++)
 			for(int j = 0; j < m; j++)
-				C[i + j] = (C[i + j] + (long long)a[i] * b[j]) % MOD;
+				C[i + j] = C[i + j] + a[i] * b[j];
 		for(int i = 0; i < n + m - 1; i++)
 			c[i] = C[i];
 	}
@@ -105,11 +84,11 @@ namespace myNTT
 		if((long long)n * m <= 2333)
 			conv_force(n, a, m, b, c);
 		else
-			conv_ntt(n, a, m, b, c);
+			conv_fft(n, a, m, b, c);
 	}
 }
 
-using myNTT::conv;
+using myFFT::conv;
 
 int n, m;
 int a[MAXN], b[MAXN], c[MAXN];
@@ -119,8 +98,7 @@ int main()
 	ios::sync_with_stdio(false);
 
 	cin >> n >> m;
-	n = n + 1, m = m + 1;
-
+	n += 1, m += 1;
 	for(int i = 0; i < n; i++)
 		cin >> a[i];
 	for(int i = 0; i < m; i++)
--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 * Bigint - 高精度
 * FastInput - 读入优化(fread)
+* FFT - 快速傅里叶变换
 * NTT - 快速数论变换
 
