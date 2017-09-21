29ed298144b55b73c68b2d31bcaaa7fbc94c4789
ct123098 <tom20000609@126.com>
2017-09-21 10:03:37 +0800

    add Dinic & modify ISAP

--- Dinic.cpp
+++ Dinic.cpp
@@ -3,14 +3,15 @@
 #include <cstdlib>
 #include <cstring>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
 const int MAXN = 100000 + 100;
-const int MAXM = 1000000 + 100;
+const int MAXM = 1000000 + 1000;
 const int INF = 0x3f3f3f3f;
 
-namespace myISAP
+namespace myDinic
 {
 	struct Edge
 	{
@@ -24,6 +25,7 @@ namespace myISAP
 		e[l] = (Edge){p[a], b, c};
 		p[a] = l++;
 	}
+
 	void ins2(int a, int b, int c)
 	{
 		addE(a, b, c);
@@ -31,56 +33,69 @@ namespace myISAP
 	}
 
 	int S, T, N;
-	int dis[MAXN], num[MAXN];
+	int dis[MAXN];
+	queue<int> q;
 	int flow = 0;
+
+	bool bfs()
+	{
+		memset(dis, 0, (N + 2) * sizeof(dis[0]));
+		dis[S] = 1, q.push(S);
+		while(!q.empty())
+		{
+			int x = q.front();
+			q.pop();
+			for(int i = p[x]; i; i = e[i].next)
+				if(e[i].w > 0 && !dis[e[i].to])
+				{
+					int y = e[i].to;
+					dis[y] = dis[x] + 1, q.push(y);
+				}
+		}
+		return dis[T];
+	}
 
 	int dfs(int x, int s)
 	{
 		if(x == T)
 			return s;
 		int ret = 0;
-		for(int &i = head[x]; i; i = e[i].next)
-			if(e[i].w > 0 && dis[e[i].to] + 1 == dis[x])
+		for(int i = head[x]; i; i = e[i].next)
+			if(e[i].w > 0 && dis[x] + 1 == dis[e[i].to])
 			{
 				int t = dfs(e[i].to, min(e[i].w, s));
 				e[i].w -= t, e[i ^ 1].w += t;
 				s -= t, ret += t;
-				if(dis[S] >= N || s <= 0)
-					return ret;
+				if(s <= 0)
+					break;
 			}
-		int ndis = N;
-		for(int i = p[x]; i; i = e[i].next)
-			if(e[i].w > 0)
-				ndis = min(ndis, dis[e[i].to] + 1);
-		if(!(--num[dis[x]])) dis[S] = N;
-		num[dis[x] = ndis]++;
-		head[x] = p[x];
 		return ret;
 	}
 
-	int isap()
+	int dinic()
 	{
-		memset(dis, 0, sizeof(dis));
-		memset(num, 0, sizeof(num));
-		num[0] = N;
-		memcpy(head, p, sizeof(p));
 		flow = 0;
-		while(dis[S] < N)
+		while(bfs())
+		{
+			memcpy(head, p, (N + 2) * sizeof(p[0]));
 			flow += dfs(S, INF);
+		}
 		return flow;
 	}
 }
 
-
 int main()
 {
 	ios::sync_with_stdio(false);
+	// freopen("1.in", "r", stdin);
+	// freopen("1.out", "w", stdout);
 
-	using namespace myISAP;
-	
+	using namespace myDinic;
+
 	int n, m;
 	cin >> n >> m;
 	S = 1, T = n, N = n;
+
 	for(int i = 1; i <= m; i++)
 	{
 		int x, y, z;
@@ -88,10 +103,9 @@ int main()
 		ins2(x, y, z);
 	}
 
-	int ans = isap();
+	int ans = dinic();
 
 	cout << ans << endl;
-
 
 	return 0;
 }
